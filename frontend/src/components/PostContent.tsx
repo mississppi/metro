86bb42385e44b387ad6e717944ca201918cdac3b
mississppi <msspstudio@gmail.com>
2024-11-03 02:57:07 +0900

    textarea

--- frontend/src/components/PostContent.tsx
+++ frontend/src/components/PostContent.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PostContent = ({ content }: {
     content: string;
 }) => {
+    // 初期値としてpropsのcontentを設定
+    const [editableContent, setEditableContent] = useState(content);
+
+    const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setEditableContent(e.target.value)
+    }
     return (
         <div className="text-base">
-            {content}
+            <textarea 
+                value={editableContent}
+                onChange={handleContentChange}
+                className="w-full h-40 p-2 border rounded"
+                placeholder='ここにメモの内容'
+            />
         </div>
     );
 };
