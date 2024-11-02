1fc006b6eba023373791ac2a8a319e8f992d8876
mississppi <msspstudio@gmail.com>
2024-11-02 12:30:02 +0900

    add layout

---
+++ frontend/src/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div className='flex'>
+            <div>左</div>
+            <div>右</div>
+        </div>
+    )
+}
+
+export default MainLayout
