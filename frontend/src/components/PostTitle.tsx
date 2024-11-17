8f8a1155c1683af0a7358a10b16c0452d10330e2
mississppi <msspstudio@gmail.com>
2024-11-17 23:20:56 +0900

    styles

--- frontend/src/components/PostTitle.tsx
+++ frontend/src/components/PostTitle.tsx
@@ -18,23 +18,37 @@ const PostTitle = ({ title, onTitleChange, onNewPost}:{
         onTitleChange(newTitle);
     }
     return (
-        <div className="flex items-center justify-between space-x-4">
+        <div className="flex items-center justify-between space-x-1">
             <input 
                 type="text"
                 value={editableTitle}
                 onChange={handleTitleChange}
                 className="h-full text-2xl font-bold  focus:outline-none bg-[#383838] text-white p-2 rounded"
             />
-            <button 
-                onClick={onNewPost} // ボタンのクリックで新規追加の処理を実行
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
-            >
-                <img 
-                    src="/icons/plus.svg" 
-                    alt="新規追加" 
-                    className="w-5 h-5" 
-                />
-            </button>
+            <div className="flex space-x-2 justify-end">
+                <button 
+                    onClick={onNewPost} // ボタンのクリックで新規追加の処理を実行
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 focus:outline-none"
+                    >
+                    <img 
+                        src="/icons/plus.svg" 
+                        alt="新規追加" 
+                        className="w-5 h-5" 
+                        />
+                </button>
+
+                    {/* 削除ボタン */}
+                <button 
+                    // onClick={onDelete}
+                    className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 focus:outline-none"
+                    >
+                    <img 
+                        src="/icons/trash.svg" 
+                        alt="削除" 
+                        className="w-5 h-5" 
+                        />
+                </button>
+            </div>
         </div>
         
     );
