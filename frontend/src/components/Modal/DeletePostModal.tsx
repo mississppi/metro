22cde79871b859d7f731d68d6fafe811af26943e
mississppi <msspstudio@gmail.com>
2024-12-08 13:53:34 +0900

    add deletemodal

--- frontend/src/components/Modal/DeletePostModal.tsx
+++ frontend/src/components/Modal/DeletePostModal.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 
-// const DeletePostModal = ({postId, isOpen, onClose, onDelete}: {
-//     postId: number;
-//     isOpen: boolean;
-//     onClose: boolean;
-//     onDelete: () => void;
-// }) => {
 const DeletePostModal = ({onClose, onDelete}: {
     onClose: () => void;
     onDelete: () => void;
 }) => {
-    // if (!isOpen) return null;
-
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
             <div className="bg-white p-6 rounded shadow-lg w-1/3">
-                <h2 className="text-lg font-semibold mb-4">Confirm Deletion</h2>
-                <p className="mb-6">
+                <h2 className="text-gray-600 text-lg font-semibold mb-4">Confirm Deletion</h2>
+                <p className="text-gray-600 mb-6">
                     Are you sure you want to delete this post? This action cannot be undone.
                 </p>
                 <div className="flex justify-end space-x-4">
